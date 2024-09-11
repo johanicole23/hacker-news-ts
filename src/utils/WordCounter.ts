64c21d50bb4ac00ec949c9e0e498c7f4464f7892
johanicole23 <johaniky12@hotmail.com>
2024-09-11 12:27:21 -0500

    Creating the class for count the words and simple testing in the index

---
+++ src/utils/WordCounter.ts
@@ -0,0 +1,13 @@
+export class WordCounter {
+    /**
+     * Method to count the words in the title of the new without special charcaters and words with - like one
+     */
+    static countWords(title: string): number {
+
+        const convertToOneWord = title.replace(/-/g, '');
+        const withoutSpecialCharacters = convertToOneWord.replace(/[^a-zA-Z\s]/g, '');
+        const words = withoutSpecialCharacters.trim().split(/\s+/);
+
+        return words.length;
+    }
+}
