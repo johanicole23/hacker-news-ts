8a42327a979ecffb4d79353156c6b05a0b5eff27
johanicole23 <johaniky12@hotmail.com>
2024-09-11 00:21:54 -0500

    Change the names of the parameters for consistency

---
+++ src/models/Entry.ts
@@ -0,0 +1,8 @@
+export class Entry{
+    constructor(
+        public entryId: number,
+        public entryTitle: string,
+        public entryPoints: number,
+        public entryComments : number
+    ) {}
+}
