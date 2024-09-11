986c74c26d7c38178dabf060c838b703a56505a7
johanicole23 <johaniky12@hotmail.com>
2024-09-11 13:56:03 -0500

    Add a class for the counting of words

--- src/utils/WordCounter.ts
+++ src/utils/WordCounter.ts
@@ -4,6 +4,10 @@ export class WordCounter {
      */
     static countWords(title: string): number {
 
+        if (title === '') return 0;
+
+        if (!/[a-zA-Z]/.test(title)) return 0;
+
         const convertToOneWord = title.replace(/-/g, '');
         const withoutSpecialCharacters = convertToOneWord.replace(/[^a-zA-Z\s]/g, '');
         const words = withoutSpecialCharacters.trim().split(/\s+/);
